28ec006e36b789e9cb214186a5c91ad533fefb22
Michal <michaldacko4@wp.pl>
2021-06-05 18:27:46 +0200

    dodanie stron

---
+++ Website/recipes/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openSidebar() {
+  document.getElementById("side_menu").style.width = "19%";
+}
+
+function closeSidebar() {
+  document.getElementById("side_menu").style.width = "0";
+}
