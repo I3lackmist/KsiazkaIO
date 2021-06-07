7ab53da8fc3ba2ae2505135f54cb84af7f99c431
Fredunio <michaldacko4@wp.pl>
2021-06-07 23:35:19 +0200

    final push

--- Website/create_recipe/script.js
+++ Website/create_recipe/script.js
@@ -7,3 +7,11 @@ function openSidebar() {
 function closeSidebar() {
   document.getElementById("side_menu").style.width = "0";
 }
+
+function hideProductDetails() {
+    document.getElementById("product_details").style.display="none";
+}
+
+function showProductDetails() {
+    document.getElementById("product_details").style.display="inline-block";
+}
